cd1f6d77cd741aaaaebac442bf18734c2f78d966
Mike Kasick <mike@kasick.org>
2011-12-01 16:14:40 -0500

    Revert nand addresses.
    Signed-off-by: nubecoder <nubecoder@gmail.com>

--- Kernel/drivers/mtd/onenand/samsung_epic.h
+++ Kernel/drivers/mtd/onenand/samsung_epic.h
@@ -7,22 +7,22 @@
 struct mtd_partition s3c_partition_info[] = {
 
  /*This is partition layout from the oneNAND it SHOULD match the pitfile on the second page of the NAND.
-   It will work if it doesn't but beware to write below the adress 0x00004800 there are the bootloaders.
+   It will work if it doesn't but beware to write below the adress 0x01200000 there are the bootloaders.
    Currently we won't map them, but we should keep that in mind for later things like flashing bootloader
-   from Linux. There is a partition 'efs' starting @ 0x00000200 40 256K pages long, it contains data for
+   from Linux. There is a partition 'efs' starting @ 0x00080000 40 256K pages long, it contains data for
    the modem like IMSI we don't touch it for now, but we need the data from it, we create a partition
    for that and copy the data from it. For this you need a image from it and mount it as vfat or copy
    it on a kernel with rfs support on the phone.
    
    Partitions on the lower NAND adresses:
    
-   0x00000000 - 0x000000FF = first stage bootloader
-   0x00000000 - 0x000001FF = PIT for second stage bootloader
-   0x00000200 - 0x000029FF = EFS: IMSI and NVRAM for the modem
-   0x00002A00 - 0x00002EFF = second stage bootloader
-   0x00002F00 - 0x000033FF = backup of the second stage bootloader (should be loaded if the other fails, unconfirmed!)
-   0x00003400 - 0x000047FF = PARAM.lfs config the bootloader
-   
+   0x00000000 - 0x0003FFFF = first stage bootloader
+   0x00040000 - 0x0007FFFF = PIT for second stage bootloader
+   0x00080000 - 0x00A7FFFF = EFS: IMSI and NVRAM for the modem
+   0x00A80000 - 0x00BBFFFF = second stage bootloader
+   0x00BC0000 - 0x00CFFFFF = backup of the second stage bootloader (should be loaded if the other fails, unconfirmed!)
+   0x00D00000 - 0x011FFFFF = PARAM.lfs config the bootloader
+
    #########################################################################################
    #########################################################################################
    ###### NEVER TOUCH THE FIRST 2 256k PAGES! THEY CONTAIN THE FIRST STAGE BOOTLOADER ######
