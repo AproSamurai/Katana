6228db1395e9f5daca69d1bb6854f9a375ae9ff3
Steven Smith <AproSamurai@gmail.com>
2012-01-27 14:08:44 -0800

    clean up build errors.

--- Kernel/arch/arm/mach-s5pv210/cpu-freq.c
+++ Kernel/arch/arm/mach-s5pv210/cpu-freq.c
@@ -923,13 +923,13 @@ static int s5pv210_cpufreq_notifier_event(struct notifier_block *this,
 }
 
 static struct freq_attr *s5pv210_cpufreq_attr[] = {
-<<<<<<< HEAD
+
 	&cpufreq_freq_attr_scaling_available_freqs,
 	NULL,
-=======
+
   &cpufreq_freq_attr_scaling_available_freqs,
   NULL,
->>>>>>> 79ddbcb... Overclock support (from rodderick's repo)
+
 };
 
 static struct cpufreq_driver s5pv210_cpufreq_driver = {
--- Kernel/drivers/usb/gadget/s3c_udc_otg.c
+++ Kernel/drivers/usb/gadget/s3c_udc_otg.c
@@ -430,20 +430,20 @@ static int s3c_udc_power(struct s3c_udc *dev, char en)
 }
 void s5p_set_otg_dvfs(int enable)
 {
-<<<<<<< HEAD
+
 	if (enable) {
 		s5pv210_lock_dvfs_high_level(DVFS_LOCK_TOKEN_8,L1); //800MHz lock
 	} else {
 		s5pv210_unlock_dvfs_high_level(DVFS_LOCK_TOKEN_8);
 	}
-=======
+
   if (enable) {
                 s5pv210_lock_dvfs_high_level(DVFS_LOCK_TOKEN_8,L5); //800MHz lock
         } else {
                 s5pv210_unlock_dvfs_high_level(DVFS_LOCK_TOKEN_8);
         }
 
->>>>>>> 79ddbcb... Overclock support (from rodderick's repo)
+
 }
 int s3c_vbus_enable(struct usb_gadget *gadget, int enable)
 {
@@ -452,7 +452,7 @@ int s3c_vbus_enable(struct usb_gadget *gadget, int enable)
 
 #if 0
 	// USB Gadget entry point
-<<<<<<< HEAD
+
 	if (enable) {
 		dev_info(&gadget->dev, "USB udc %d,%d lock\n", dev->udc_enabled, enable);
 		s5pv210_lock_dvfs_high_level(DVFS_LOCK_TOKEN_8,L1); //800MHz lock
@@ -460,7 +460,7 @@ int s3c_vbus_enable(struct usb_gadget *gadget, int enable)
 		dev_info(&gadget->dev, "USB udc %d,%d unlock\n", dev->udc_enabled, enable);
 		s5pv210_unlock_dvfs_high_level(DVFS_LOCK_TOKEN_8);
 	}
-=======
+
 	 if (enable) {
   		dev_info(&gadget->dev, "USB udc %d,%d lock\n", dev->udc_enabled, enable);
   		s5pv210_lock_dvfs_high_level(DVFS_LOCK_TOKEN_8,L5); //800MHz lock
@@ -468,7 +468,7 @@ int s3c_vbus_enable(struct usb_gadget *gadget, int enable)
   		dev_info(&gadget->dev, "USB udc %d,%d unlock\n", dev->udc_enabled, enable);
   		s5pv210_unlock_dvfs_high_level(DVFS_LOCK_TOKEN_8);
  	}
->>>>>>> 79ddbcb... Overclock support (from rodderick's repo)
+
 #endif
 
 	if (dev->udc_enabled != enable) {
@@ -995,6 +995,7 @@ static const struct usb_gadget_ops s3c_udc_ops = {
 };
 
 static void nop_release(struct device *dev)
+
 {
 	DEBUG("%s\n", __func__);
 }
